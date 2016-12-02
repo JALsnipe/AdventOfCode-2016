db6fbf7827ebdbe7bfb65a3b002c89d35e7eddee
Josh Lieberman <josh.lieberman92@gmail.com>
2016-12-02 01:47:29 -0500

    Day 2 Gold

--- day2.swift
+++ day2.swift
@@ -9,27 +9,18 @@ enum Direction: Character {
 
 struct Bathroom {
     
-    let pinpad = [[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 9]]
+    // let pinpad = [[1, 2, 3],
+    //               [4, 5, 6],
+    //               [7, 8, 9]]
     
-    var position = (x: 1, y: 1) {
+    let pinpad = [[0, 0, 1, 0, 0],
+                  [0, 2, 3, 4, 0],
+                  [5, 6, 7, 8, 9],
+                  [0, "A", "B", "C", 0],
+                  [0, 0, "D", 0, 0]]
+    
+    var position = (x: 0, y: 2) {
         didSet {
-            if position.x < 0 {
-                position.x = 0
-            }
-            
-            if position.x > 2 {
-                position.x = 2
-            }
-            
-            if position.y < 0 {
-                position.y = 0
-            }
-            
-            if position.y > 2 {
-                position.y = 2
-            }
         }
     }
     
@@ -39,6 +30,12 @@ struct Bathroom {
     "RDDRUDLRLDDDRLRRLRRLUULDRLRUUURULRRLUURLLLRLULDDLDLRLULULUUDDDRLLLUDLLRUDURUDDLLDUDLURRULLRDLDURULRLDRLDLDRDDRUDRUULLLLRULULLLDDDULUUDUUDDLDRLRRDLRLURRLLDRLDLDLULRLRDLDLRLUULLDLULRRRDDRUULDUDLUUUUDUDRLUURDURRULLDRURUDURDUULRRULUULULRLDRLRLLRRRLULURLUDULLDRLDRDRULLUUUDLDUUUDLRDULRDDDDDDDDLLRDULLUDRDDRURUDDLURRUULUURURDUDLLRRRRDUDLURLLURURLRDLDUUDRURULRDURDLDRUDLRRLDLDULRRUDRDUUDRLURUURLDLUDLLRDDRDU\n" +
     "LLDDDDLUDLLDUDURRURLLLLRLRRLDULLURULDULDLDLLDRRDLUDRULLRUUURDRLLURDDLLUDDLRLLRDDLULRLDDRURLUDRDULLRUDDLUURULUUURURLRULRLDLDDLRDLDLLRUURDLUDRRRDDRDRLLUDDRLDRLLLRULRDLLRLRRDDLDRDDDUDUDLUULDLDUDDLRLDUULRULDLDULDDRRLUUURUUUDLRDRULDRRLLURRRDUDULDUDUDULLULLULULURLLRRLDULDULDLRDDRRLRDRLDRLUDLLLUULLRLLRLDRDDRUDDRLLDDLRULLLULRDDDLLLDRDLRULDDDLULURDULRLDRLULDDLRUDDUDLDDDUDRDRULULDDLDLRRDURLLRLLDDURRLRRULLURLRUDDLUURULULURLRUDLLLUDDURRLURLLRLLRRLDULRRUDURLLDDRLDLRRLULUULRRUURRRDULRLRLRDDRDULULUUDULLLLURULURRUDRLL\n" +
     "UULLULRUULUUUUDDRULLRLDDLRLDDLULURDDLULURDRULUURDLLUDDLDRLUDLLRUURRUDRLDRDDRRLLRULDLLRUUULLLDLDDULDRLRURLDRDUURLURDRUURUULURLRLRRURLDDDLLDDLDDDULRUDLURULLDDRLDLUDURLLLLLRULRRLLUDRUURLLURRLLRDRLLLRRDDDRRRDLRDRDUDDRLLRRDRLRLDDDLURUUUUULDULDRRRRLUDRLRDRUDUDDRULDULULDRUUDUULLUDULRLRRURDLDDUDDRDULLUURLDRDLDDUURULRDLUDDLDURUDRRRDUDRRDRLRLULDRDRLRLRRUDLLLDDDRURDRLRUDRRDDLDRRLRRDLUURLRDRRUDRRDLDDDLRDDLRDUUURRRUULLDDDLLRLDRRLLDDRLRRRLUDLRURULLDULLLUDLDLRLLDDRDRUDLRRDDLUU"
+    
+    // let instructions = "ULL\n" +
+    //                   "RRDDD\n" +
+    //                   "LURDL\n" +
+    //                   "UUUUD"
+                   
                    
     let password = [Int]()
     
@@ -62,16 +59,80 @@ struct Bathroom {
     mutating func move(direction: Direction) {
         switch direction {
             case .up:
+                let oldY = position.y
                 position.y -= 1
+                
+                // range check
+                if position.x > 4 ||
+                   position.x < 0 ||
+                   position.y > 4 ||
+                   position.y < 0 {
+                    // out of bounds, use old value
+                    position.y = oldY
+                }
+                
+                let key = pinpad[position.x][position.y]
+                
+                if key as? Int == 0 {
+                    position.y = oldY
+                }
             
             case .down:
+                let oldY = position.y
                 position.y += 1
+                
+                // range check
+                if position.x > 4 ||
+                   position.x < 0 ||
+                   position.y > 4 ||
+                   position.y < 0 {
+                    // out of bounds, use old value
+                    position.y = oldY
+                }
+                
+                let key = pinpad[position.x][position.y]
+                
+                if key as? Int == 0 {
+                    position.y = oldY
+                }
             
             case .left:
+                let oldX = position.x
                 position.x -= 1
                 
+                // range check
+                if position.x > 4 ||
+                   position.x < 0 ||
+                   position.y > 4 ||
+                   position.y < 0 {
+                    // out of bounds, use old value
+                    position.x = oldX
+                }
+                
+                let key = pinpad[position.x][position.y]
+                
+                if key as? Int == 0 {
+                    position.x = oldX
+                }
+                
             case .right:
+                let oldX = position.x
                 position.x += 1
+                
+                // range check
+                if position.x > 4 ||
+                   position.x < 0 ||
+                   position.y > 4 ||
+                   position.y < 0 {
+                    // out of bounds, use old value
+                    position.x = oldX
+                }
+                
+                let key = pinpad[position.x][position.y]
+                
+                if key as? Int == 0 {
+                    position.x = oldX
+                }
         }
     }
     
